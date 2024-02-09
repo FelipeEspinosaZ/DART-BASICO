a2d945b385e64441429604ed45bc3d6e1a6e3117
juanca2805 <juan.pedraza2805@gmail.com>
2024-02-08 19:28:04 -0500

    first commit

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,35 @@
+void main(List<String> arguments) {
+  print('hola mundo');
+
+  String varTexto = "hola mundo";
+  int numEntero = 15;
+  double numeroDouble = 12.2;
+  bool esVerdad = true;
+
+  var esUnTexto = "Texto";
+
+  dynamic esUnNumero = 14;
+  esUnNumero = 'hola';
+
+  print(varTexto + '  ' + esUnNumero);
+
+  String? name;
+
+  var arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 0];
+
+  List<int> lista = [1, 2, 3, 4, 5, 6];
+
+  List<String> ListaString = ["", "", "", "", "", ""];
+
+  if (numEntero == 15) {}
+
+  switch (expression) {
+    case value:
+      break;
+    default:
+  }
+
+  for (var i = 0; i < count; i++) {}
+
+  while (condition) {}
+}
